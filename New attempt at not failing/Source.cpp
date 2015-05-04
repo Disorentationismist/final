0b2b291ac7b82ac6f06fc48a6e4118fc6206653f
SPARTAN62597 <spartan62597@gmail.com>
2015-05-04 07:55:36 -0400

    Changes
    Working program, its a start.

--- New attempt at not failing/Source.cpp
+++ New attempt at not failing/Source.cpp
@@ -1,88 +1,54 @@
 //Sql.cpp
-#include  <windows.h>
-#include  <string>
-#include  <cstdio>
-#include  <odbcinst.h>
-#include  <sqltypes.h>
-#include  <sql.h>
-#include  <sqlext.h>
-#include  "MySql.h"
-
-
-SQL::SQL() //Constructor
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+using namespace std;
+struct work
 {
-	ZeroMemory(szCnOut, 512);
-	ZeroMemory(szErrMsg, 512);
-	strDBQ = "";
-}
-
-
-SQL::~SQL()
+	char name[100];
+	int id;
+	char job[10];
+}workers[10];
+int main()
 {
-	//Sql Destructor
-}
-
-
-void SQL::MakeConnectionString(void)
-{
-	if (strDriver == "SQL Server")
+	int numWorkers = 0;
+	int option = 0;
+	ofstream file;
+	while (1)
 	{
-		if (strDBQ == "")
+		cout << "\nThe c++ Database application\n1-To enter the data\n2-To read the data\n3-To save the data\n";
+		cin >> option;
+		switch (option)
 		{
-			strConnectionString = "DRIVER=";
-			strConnectionString = strConnectionString + strDriver + ";" + "SERVER=" + strServer + ";";
+		case 1:
+			cout << "Enter the no. of workers:" << endl;
+			cin >> numWorkers;
+			for (int i = 0; i < numWorkers; i++)
+			{
+				cout << endl << "Enter the name of the worker:" << endl;
+				cin >> workers[i].name;
+				cout << endl << "Enter the ID no. of the worker:" << endl;
+				cin >> workers[i].id;
+				cout << endl << "Enter the job of the worker:" << endl;
+				cin >> workers[i].job;
+				cout << endl;
+			}
+			break;
+		case 2:
+			for (int i = 0; i < numWorkers; i++)
+				cout << endl << "The worker no. " << i + 1 << " is " << workers[i].name << " with an ID no. of " << workers[i].id;
+			cout << endl << endl;
+			break;
+		case 3:
+			file.open("report.txt");
+			for (int i = 0; i < numWorkers; i++)
+				file << endl << "The worker no. " << i + 1 << " is " << workers[i].name << " with an ID no. of " << workers[i].id << endl;
+			file << endl;
+			file.close();
+			cout << "The file is saved" << endl;
+			_getch();
+			break;
 		}
-		else
-		{
-			strConnectionString = "DRIVER=";
-			strConnectionString = strConnectionString + strDriver + ";" + "SERVER=" + strServer + ";" + \
-				"DATABASE=" + strDatabase + ";" + "DBQ=" + strDBQ + ";";
-		}
 	}
-	else if (strDriver == "Microsoft Access Driver (*.mdb)")
-	{
-		strConnectionString = "DRIVER=";
-		strConnectionString = strConnectionString + strDriver + ";" + "DBQ=" + strDBQ + ";";
-	}
-	else if (strDriver == "Microsoft Excel Driver (*.xls)")
-	{
-		strConnectionString = "DRIVER=";
-		strConnectionString = strConnectionString + strDriver + ";" + "DBQ=" + strDBQ + ";";
-	}
-}
-
-
-void SQL::ODBCConnect(void)
-{
-	char szCnIn[512];
-	unsigned int iResult;
-
-	MakeConnectionString();
-	SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnvr);
-	SQLSetEnvAttr(hEnvr, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, SQL_IS_INTEGER);
-	SQLAllocHandle(SQL_HANDLE_DBC, hEnvr, &hConn);
-	strcpy_s(szCnIn, strConnectionString.c_str());
-	iResult = SQLDriverConnect(hConn, NULL, (SQLTCHAR*)szCnIn, (SQLSMALLINT)strlen(szCnIn), (SQLTCHAR*)szCnOut, 512, &swStrLen, SQL_DRIVER_NOPROMPT);
-	if (iResult == 0 || iResult == 1)
-		blnConnected = TRUE;
-	else
-	{
-		SQLGetDiagRec(SQL_HANDLE_DBC, hConn, 1, szErrCode, &iNativeErrPtr, szErrMsg, 512, &iTextLenPtr);
-		blnConnected = FALSE;
-		SQLDisconnect(hConn);
-		SQLFreeHandle(SQL_HANDLE_DBC, hConn);
-		SQLFreeHandle(SQL_HANDLE_ENV, hEnvr);
-	}
-}
-
-
-void SQL::ODBCDisconnect(void)
-{
-	if (blnConnected == TRUE)
-	{
-		SQLDisconnect(hConn);
-		SQLFreeHandle(SQL_HANDLE_DBC, hConn);
-		SQLFreeHandle(SQL_HANDLE_ENV, hEnvr);
-		blnConnected = FALSE;
-	}
+	return 0;
 }
